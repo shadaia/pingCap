640fb153239b79a4c20bd5e22a7548a665f59f6f
imooc <1065656280@qq.com>
2020-08-28 15:06:58 +0800

    first commit

---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) throws Exception {
+        Solution.init();
+        Solution solution=new Solution();
+        solution.find("b",2);
+    }
+}
